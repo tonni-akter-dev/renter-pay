850aef9e1317e92aa0f574485fddbd80dec4f269
Md Junaead <tanniakhter123@gmail.com>
2025-10-16 18:25:19 +0600

    update blog details

--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import BlogCard from "@/app/(home)/blog/components/BlogCard";
-import BioData from "./components/data";
+import { bioData } from "@/app/utils/data";
 const Blog = () => {
   return (
     <div>
       <h2 className="text-4xl font-semibold text-center">Our Blogs</h2>
       <p className="text-lg text-center mb-10">Read our recent blogs</p>
-      <div className="grid grid-cols-2 gap-4">
-       
-      {BioData.map((blog)=>(<BlogCard key={blog.id} blog={blog}/>))}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      {bioData.map((blog)=>(<BlogCard key={blog.id} blog={blog}/>))}
       </div>
-     
     </div>
   );
 };
