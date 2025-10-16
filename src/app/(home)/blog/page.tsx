937139068e9082facfeda72754b6d75b91f86398
reazhossain950 <reazhossain950@gmail.com>
2025-10-16 18:18:45 +0600

    blog page updated

--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import BlogCard from "@/app/(home)/blog/components/BlogCard";
+import BioData from "./components/data";
 const Blog = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <h2 className="text-4xl font-semibold text-center">Our Blogs</h2>
+      <p className="text-lg text-center mb-10">Read our recent blogs</p>
+      <div className="grid grid-cols-2 gap-4">
+       
+      {BioData.map((blog)=>(<BlogCard key={blog.id} blog={blog}/>))}
+      </div>
+     
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
